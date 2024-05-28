de2cbdbe02b375a0ac90ddcce0cbc7f8628f72df
SantiagoProgramador <andresx1277@gmail.com>
2024-05-28 09:21:06 -0500

    Working on the endpoints configuration

--- src/main/java/com/Santiago/mockTest/api/Controllers/SubmissionController.java
+++ src/main/java/com/Santiago/mockTest/api/Controllers/SubmissionController.java
@@ -1,5 +1,14 @@
 package com.Santiago.mockTest.api.Controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping(path = "/submissions")
+@AllArgsConstructor
 public class SubmissionController {
-  
+
 }
